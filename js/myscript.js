bc8fa12211fb5b56014703967ea02588a570a2ce
Angga Risky <anggariskysetiawan@gmail.com>
2017-04-05 11:51:43 -0700

    first push from revision

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+function megamenuPanda()
+{
+	var dropdown = document.getElementById("mm-panda");
+	dropdown.style.display = "block";
+}
+
+function closeMMPanda()
+{
+	var dropdown = document.getElementById("mm-panda");
+	dropdown.style.display = "none";
+}
